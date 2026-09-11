6434a1165e45b800bfe822823e4b884c328d253c
MarcosJn08 <mjrs2207@gmail.com>
2026-09-11 15:27:38 -0300

    [#39] [FRONTEND] Configuração de Assets, Paleta de Cores e Fontes

---
+++ frontend/src/components/Formulario.jsx
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function Formulario() {
+  return (
+    <h1>Meu Site</h1>
+  );
+}
+
+export default Formulario;
